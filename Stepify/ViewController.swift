ec9b0d3e0d5f7c8c35fc8deb4354faebafbbdc9e
Jorge Zapata <jorgejaden@gmail.com>
2020-09-23 20:28:47 +0200

    Initial Commit

---
+++ Stepify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stepify
+//
+//  Created by JJ Zapata on 9/23/20.
+//  Copyright © 2020 Jorge Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
